57a13cf19314af824ee077aed2553be31a152b42
SHUBHAM GUPTA <gshubham804@gmail.com>
2024-06-07 16:28:12 +0530

    UI completed , responsive left

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -7,16 +7,16 @@ const Navbar = () => {
     threshold: 0.1,
   });
   return (
-    <nav  ref={topRef} className={`pt-16 ${inViewTop ? 'animate-slideInFromTop' : ''} `}>
+    <nav  ref={topRef} className={`navbar-react pt-16 ${inViewTop ? 'animate-slideInFromTop' : ''} `}>
       <div className="container mx-auto flex items-center justify-around ">
-          <p className="text-white font-semibold text-2xl cursor-pointer border-t-[1px]">Ecosystem</p>
-          <p className="text-white font-semibold text-2xl cursor-pointer border-t-[1px]">Developers</p>
-          <div className="flex items-center space-x-2 -mt-16 border-b-[1px] border-x-[1px]">
+          <p className="text-white font-semibold text-2xl cursor-pointer ">Ecosystem</p>
+          <p className="text-white font-semibold text-2xl cursor-pointer ">Developers</p>
+          <div className="flex items-center space-x-2 -mt-8 mr-20">
             <img src={LogoImage} alt="Logo" className="h-16 w-auto" />
             <p className="text-white text-3xl font-semibold cursor-pointer">Polluxcoin</p>
           </div>
-          <p className="text-white font-semibold text-2xl cursor-pointer border-t-[1px]">Ecosystem</p>
-          <p className="text-white font-semibold text-2xl cursor-pointer border-t-[1px]">Coin</p>
+          <p className="text-white font-semibold text-2xl cursor-pointer ">Ecosystem</p>
+          <p className="text-white font-semibold text-2xl cursor-pointer ">Coin</p>
       </div>
     </nav>
   );
