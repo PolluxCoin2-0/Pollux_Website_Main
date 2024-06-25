895d69427e2339848469f0e70226e95db050e5b0
SHUBHAM GUPTA <gshubham804@gmail.com>
2024-06-25 13:44:43 +0530

    added navbar options

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -71,9 +71,21 @@ const Navbar = () => {
         </div>
         {isMobile && (
           <div className="absolute right-0 top-full mt-2 bg-[#170535] rounded-md shadow-lg px-12 py-4 z-10">
-            <p className="text-white font-semibold text-lg cursor-pointer mb-2">
+            <p className="text-white font-semibold text-lg cursor-pointer mb-2" 
+              onClick={toggleEcosystem}
+              >
               Ecosystem
             </p>
+            {isEcosystemOpen && (
+              <div className="absolute left-0 mt-2 z-20">
+                <ul className="bg-[#230C44] rounded-md shadow-lg px-10 py-2 transition transform duration-300 ease-out opacity-100 translate-y-0 text-white">
+                  <li className="py-1">POX SR</li>
+                  <li className="py-1">Wallet</li>
+                  <li className="py-1">Explorer</li>
+                  <li className="py-1">Extension</li>
+                </ul>
+              </div>
+            )}
             <p className="text-white font-semibold text-lg cursor-pointer mb-2">
               Developers
             </p>
