9b75aff0e45b5686e105be2ea9a84a9cdeacf596
gshubham804 <gshubham804@gmail.com>
2024-07-18 13:49:51 +0530

    ...

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,32 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { lazy, Suspense } from "react";
+import { lazy, Suspense, useState, useEffect } from "react";
 import Footer from "./layout/Footer";
 
 const Home = lazy(() => import("./pages/Home"));
 
+const LoadingScreen = () => {
+  return (
+    <div>Loading...</div>
+  );
+};
 function App() {
+  const [footerVisible, setFooterVisible] = useState(false);
+
+  const handleHomeLoad = () => {
+    setTimeout(() => {
+      setFooterVisible(true);
+    }, 2000); // Set footer visibility after 2 seconds
+  };
+
   return (
     <div className="bg-[#10022B]">
       <Router>
-        <Suspense fallback={<div>Loading...</div>}>
+        <Suspense fallback={<LoadingScreen />}>
           <Routes>
-            <Route path="/" element={<Home />} />
+            <Route path="/" element={<Home onLoad={handleHomeLoad} />} />
           </Routes>
-          <Footer />
         </Suspense>
+        {footerVisible && <Footer />}
       </Router>
     </div>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import { lazy, Suspense } from "react";
+import React, { lazy, Suspense, useEffect, useState } from "react";
 
 const Frame1 = lazy(() => import("../components/Home/Frame1"));
 const Frame2 = lazy(() => import("../components/Home/Frame2/Frame2"));
@@ -8,7 +8,11 @@ const Frame5 = lazy(() => import("../components/Home/Frame5/Frame5"));
 const Frame6 = lazy(() => import("../components/Home/Frame6"));
 const Frame7 = lazy(() => import("../components/Home/Frame7/Frame7"));
 
-const Home = () => {
+const Home = ({ onLoad }) => {
+  useEffect(() => {
+    onLoad(); // Signal that the Home component has loaded
+  }, [onLoad]);
+
   return (
     <Suspense fallback={<div>Loading...</div>}>
       <Frame1 />
